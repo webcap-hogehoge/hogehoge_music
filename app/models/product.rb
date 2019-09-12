10f74fce071958548b728d1b6d2980e26ee32949
suzutatsu <suzuta1221@gmail.com>
2019-09-12 12:36:12 +0000

    finish seed inplementation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
 class Product < ApplicationRecord
+mount_uploader :image_id, ImageIdUploader
+validates :image_id, presence: true
+validates :price, presence: true
+validates :product_status, presence: true
+validates :cd_name, presence: true
+validates :is_deleted, presence: true
+
+
+enum product_status:{on_sale: 0, not_on_sale: 1}
+# on_sale = 販売中  not_on_sale = 販売停止中
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create!(image_id: open("#{Rails.root}/db/fixture/img1.jpg"), price: 1000, product_status: 1, cd_name: "天気の子", is_deleted: 0)
+Product.create!(image_id: open("#{Rails.root}/db/fixture/img2.jpg"), price: 2000, product_status: 0, cd_name: "new chalenger", is_deleted: 0)
+Product.create!(image_id: open("#{Rails.root}/db/fixture/img3.jpg"), price: 3000, product_status: 1, cd_name: "こんなに", is_deleted: 0)
+
+100.times do |n|
+	image_id = open("#{Rails.root}/db/fixture/img3.jpg")
+	cd_name = Faker::Music.album
+	price = Random.rand(1000..3000)
+	product_status = Random.rand(0..1)
+	is_deleted = Random.rand(0..1)
+	Product.create!(image_id: image_id,
+		            price: price,
+		            product_status: product_status,
+		            cd_name: cd_name,
+		            is_deleted: is_deleted,
+		            )
+end
