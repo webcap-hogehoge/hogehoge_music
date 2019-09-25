a41a310fadaf3618dac25f4393a1f9bfc4eb93d6
mai-hasunuma <mai.tomimura@gmail.com>
2019-09-25 02:49:20 +0000

    ModifyOrderHistoryCss

--- app/controllers/order_histories_controller.rb
+++ app/controllers/order_histories_controller.rb
@@ -1,21 +1,26 @@
 class OrderHistoriesController < ApplicationController
 before_action :authenticate_end_user!
+
   def new
-    @q = Product.ransack(params[:q])
-    @products = @q.result(distinct: true)
-    @address_info = params[:address_info] if params.presence
     @end_user = current_end_user
     @cart_items = @end_user.cart_items
-    @order_history = OrderHistory.new
+    if @cart_items.blank?
+      redirect_to root_path
+    else
+      @q = Product.ransack(params[:q])
+      @products = @q.result(distinct: true)
+      @address_info = params[:address_info] if params.presence
+      @order_history = OrderHistory.new
     # URLにパラメーターがあるならば、パラーメーターの情報を住所入力欄に入れる
-     if params.presence
-        @order_history.last_name_kanji = params[:last_name_kanji]
-        @order_history.first_name_kanji = params[:first_name_kanji]
-        @order_history.postal_code_1 = params[:postal_code_1]
-        @order_history.postal_code_2 = params[:postal_code_2]
-        @order_history.address = params[:address]
-        @order_history.telephone_number = params[:telephone_number]
-     end
+       if params.presence
+          @order_history.last_name_kanji = params[:last_name_kanji]
+          @order_history.first_name_kanji = params[:first_name_kanji]
+          @order_history.postal_code_1 = params[:postal_code_1]
+          @order_history.postal_code_2 = params[:postal_code_2]
+          @order_history.address = params[:address]
+          @order_history.telephone_number = params[:telephone_number]
+        end
+    end
     @end_user_fitst = @end_user.addresses.first
   end
 
@@ -44,67 +49,95 @@ before_action :authenticate_end_user!
       @order_history.subtotal = subtotal
       @order_history.total_price = @order_history.subtotal + @order_history.tax + @order_history.delivery_fee
 
-      if  @order_history.save
-          @cart_items.each do |cart_item|
-          @order_detail = OrderDetail.new
-          @order_detail.order_history_id = @order_history.id
-          @order_detail.product_id = cart_item.product_id
-          @order_detail.product_number = cart_item.product_number
-          @order_detail.unit_price = cart_item.product.price
-          @order_detail.save
-          # 実装前にコメントアウト外すこと！！！！！！！
-          #cart_item.destroy
-        end
-         redirect_to order_histories_thanks_path
+      # 登録済みでない住所に送付の場合はaddressに登録する
+      @address_info = params[:order_history][:selected_order_histories_address_id]
+
+      #binding.pry
+      if @address_info == 'address新規登録'
+        @address = Address.new
+        @address.end_user_id = current_end_user.id
+        @address.last_name = @order_history.last_name_kanji
+        @address.first_name = @order_history.first_name_kanji
+        @address.postal_code_1 = @order_history.postal_code_1
+        @address.postal_code_2 = @order_history.postal_code_2
+        @address.address = @order_history.address
+        @address.telephone_number = @order_history.telephone_number
+        @address.is_main = 0
+        @address.save
       end
+
+      @order_history.save
+      @cart_items.each do |cart_item|
+        @order_detail = OrderDetail.new
+        @order_detail.order_history_id = @order_history.id
+        @order_detail.product_id = cart_item.product_id
+        @order_detail.product_number = cart_item.product_number
+        @order_detail.unit_price = cart_item.product.price
+        @order_detail.save
+        cart_item.destroy
+      end
+      redirect_to order_histories_thanks_path
     end
   end
 
   def confirm
-    @q = Product.ransack(params[:q])
-    @products = @q.result(distinct: true)
     @end_user = current_end_user
     @cart_items = @end_user.cart_items
-    # わざと一行増やして変数を説明的にすることで見やすくする
-    @address_info = params[:order_history][:selected_order_histories_address_id]
-    # ストロングパラメーターでデータを受け取る
-    para = order_histories_params
-    # viewから送られてきたparamsの中にある:order_histories_addressの値で条件分岐
-    # newが入ってたら新規作成
-    if @address_info == 'address新規登録'
-      #全ての値が入っている場合はorder_historyを作成するための箱を用意する
-      if para[:last_name_kanji].empty? or para[:first_name_kanji].empty? or para[:telephone_number].empty? or para[:postal_code_1].empty? or para[:postal_code_2].empty? or para[:address].empty?
-          redirect_to new_order_history_path(last_name_kanji: para[:last_name_kanji], first_name_kanji: para[:first_name_kanji], telephone_number: para[:telephone_number], postal_code_1: para[:postal_code_1], postal_code_2: para[:postal_code_2], address: para[:address], pay_method: para[:pay_method])
-      #上記でない場合はnew画面で入力した情報は入れてnew 画面に戻る
-      else
-        @order_history = @end_user.order_histories.build(para)
-
-      end
-      # 数字だったら（address.id）Addressesテーブルからfind
+    if @cart_items.blank?
+      redirect_to root_path
     else
-      selected_address = Address.find(@address_info)
-      @order_history = @end_user.order_histories.build(
-        last_name_kanji: selected_address.last_name,
-        first_name_kanji: selected_address.first_name,
-        postal_code_1: selected_address.postal_code_1,
-        postal_code_2: selected_address.postal_code_2,
-        address: selected_address.address,
-        telephone_number: selected_address.telephone_number,
-        pay_method: para[:pay_method]
-        )
+      @q = Product.ransack(params[:q])
+      @products = @q.result(distinct: true)
+      @end_user = current_end_user
+      @cart_items = @end_user.cart_items
+      # わざと一行増やして変数を説明的にすることで見やすくする
+      @address_info = params[:order_history][:selected_order_histories_address_id]
+          #binding.pry
+      # ストロングパラメーターでデータを受け取る
+      para = order_histories_params
+      # viewから送られてきたparamsの中にある:order_histories_addressの値で条件分岐
+      # newが入ってたら新規作成
+      if @address_info == 'address新規登録'
+        #全ての値が入っている場合はorder_historyを作成するための箱を用意する
+        if para[:last_name_kanji].empty? or para[:first_name_kanji].empty? or para[:telephone_number].empty? or para[:postal_code_1].empty? or para[:postal_code_2].empty? or para[:address].empty?
+            redirect_to new_order_history_path(last_name_kanji: para[:last_name_kanji], first_name_kanji: para[:first_name_kanji], telephone_number: para[:telephone_number], postal_code_1: para[:postal_code_1], postal_code_2: para[:postal_code_2], address: para[:address], pay_method: para[:pay_method])
+        #上記でない場合はnew画面で入力した情報は入れてnew 画面に戻る
+        else
+          @order_history = @end_user.order_histories.build(para)
+
+        end
+        # 数字だったら（address.id）Addressesテーブルからfind
+      else
+        selected_address = Address.find(@address_info)
+        @order_history = @end_user.order_histories.build(
+          last_name_kanji: selected_address.last_name,
+          first_name_kanji: selected_address.first_name,
+          postal_code_1: selected_address.postal_code_1,
+          postal_code_2: selected_address.postal_code_2,
+          address: selected_address.address,
+          telephone_number: selected_address.telephone_number,
+          pay_method: para[:pay_method]
+          )
+      end
     end
   end
 
   def thanks
+    @end_user = current_end_user
+    @cart_items = @end_user.cart_items
+    if @cart_items.blank?
+      redirect_to root_path
+    else
     @q = Product.ransack(params[:q])
     @products = @q.result(distinct: true)
+    end
   end
 
   def index
     @q = Product.ransack(params[:q])
     @products = @q.result(distinct: true)
     @end_user = current_end_user
-    @order_histories = @end_user.order_histories.order(id: "ASC")
+    @order_histories = @end_user.order_histories.order('id DESC')
   end
 
   def show
